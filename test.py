666b497b9809fba8b2850a4c45b0afaf4bdce914
sim1222 <kousuke11236124@gmail.com>
2021-08-21 00:19:58 +0900

    wip

--- test.py
+++ test.py
@@ -34,7 +34,7 @@ while True:
     for i in range(8):
       execcom = 'his' + str(i) + ' = GPIO.input(sw)'
       exec(execcom)
-      print(GPIO.input(sw))
+      #print(GPIO.input(sw))
 
       if his0 == his1 == his4 == his5 == 0 and his2 == his3 == his6 == his7 == 1:
         PinPonDetect()
